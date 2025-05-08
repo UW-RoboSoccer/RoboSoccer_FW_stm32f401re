88a314388518efacbf7d606e0ce170fe06190e74
b57zheng <b57zheng@uwaterloo.ca>
2025-05-08 12:53:43 -0400

    System Set Up
    Added freerots and support for force sensing resistors.

---
+++ Core/Inc/adc_driver.h
@@ -0,0 +1,20 @@
+/*
+ * adc_driver.h
+ *
+ *  Created on: May 6, 2025
+ *      Author: Bowen
+ */
+
+#ifndef INC_ADC_DRIVER_H_
+#define INC_ADC_DRIVER_H_
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+#define 	ADC_CHANNEL_COUNT				1
+#define		ADC_FILTER_SIZE					5
+#define		ADC_TIMEOUT							2
+
+
+#endif /* INC_ADC_DRIVER_H_ */
